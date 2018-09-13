efb649a42ccaa956ac5ea920e7866030cdb4a032
Han <hanshengzhao1993@gmail.com>
2018-09-13 09:35:57 -0700

    refactor components

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -1,33 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../stylesheet/repolist.css';
 
-class RepoList extends Component {
-  render() {
-    return (
-      <div>
-        <h3 className="titleName">
-          Repository List:
-        </h3>
 
-        { this.props.repos.length === 0 ? (<div> No Repository Found </div>)
-          : (<ul>
-              {this.props.repos.map((ele, idx)=>{
-                return (
-                  <li key={idx} className="repoItem">
-                    <div> <span className="boldItem"> Name:</span> {ele.name} </div>
-                    <div> <span className="boldItem"> Description:</span> {ele.description} </div>
-                    <div> <span className="boldItem"> Git URL:</span> {ele.git_url} </div>
-                    <div> <span className="boldItem"> Stars:</span> {ele.stargazers_count} </div>
-                    <div> <span className="boldItem"> Forks:</span> {ele.forks} </div>
-                    <div> <span className="boldItem"> Open Issues:</span> {ele.open_issues} </div>
-                    <div> <span className="boldItem"> Repository Size:</span> {ele.size} </div>
-                  </li>)
-              })}
-            </ul>)
-        }
-      </div>
-    )
-  }
-};
+const RepoList = ({repos}) => {
+  return (
+    <div>
+      <h3 className="titleName">
+        Repository List:
+      </h3>
+
+      { repos.length === 0 ? (<div> No Repository Found </div>)
+        : (<ul>
+            {repos.map((ele, idx)=>{
+              return (
+                <li key={idx} className="repoItem">
+                  <div> <span className="boldItem"> Name:</span> {ele.name} </div>
+                  <div> <span className="boldItem"> Description:</span> {ele.description} </div>
+                  <div> <span className="boldItem"> Git URL:</span> {ele.git_url} </div>
+                  <div> <span className="boldItem"> Stars:</span> {ele.stargazers_count} </div>
+                  <div> <span className="boldItem"> Forks:</span> {ele.forks} </div>
+                  <div> <span className="boldItem"> Open Issues:</span> {ele.open_issues} </div>
+                  <div> <span className="boldItem"> Repository Size:</span> {ele.size} </div>
+                </li>)
+            })}
+          </ul>)
+      }
+    </div>
+  );
+}
 
 export default RepoList;
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,33 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../stylesheet/userprofile.css';
 
-class UserProfile extends Component {
-  render() {
-    const {userData} = this.props;
-    return (
-      <div className="container alignCenter">
-        <div className="row">
-          <div className="col">
-            <img className="image" src={userData.avatar_url} alt="User Profile" />
+const UserProfile = ({userData}) => {
+  return (
+    <div className="container alignCenter">
+      <div className="row">
+        <div className="col">
+          <img className="image" src={userData.avatar_url} alt="User Profile" />
+        </div>
+
+        <div className="col">
+          <div>
+            <b>Name:</b> {userData.name}
           </div>
 
-          <div className="col">
-            <div>
-              <b>Name:</b> {userData.name}
-            </div>
+          <div>
+            <b>Followers:</b> {userData.followers}
+          </div>
 
-            <div>
-              <b>Followers:</b> {userData.followers}
-            </div>
-
-            <div>
-              <b>Following:</b> {userData.following}
-            </div>
+          <div>
+            <b>Following:</b> {userData.following}
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
 };
 
 export default UserProfile;
